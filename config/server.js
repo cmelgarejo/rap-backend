799d33ecb4dc8190275d37a394a99c98e034b7e6
cmelgarejo <cmelgarejo.dev@gmail.com>
2022-01-23 12:42:01 -0300

    server js url
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  url: env("SERVER_URL", "localhost"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'super-jwt-secret-key'),
+      secret: env("ADMIN_JWT_SECRET", "super-jwt-secret-key"),
     },
   },
 });
